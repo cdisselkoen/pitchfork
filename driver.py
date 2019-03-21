6a71578bcf091b5af3b84860c81731ee259acffe
Craig Disselkoen <craigdissel@gmail.com>
2019-03-21 14:56:26 -0700

    troubleshooting utilities

--- driver.py
+++ driver.py
@@ -249,13 +249,38 @@ class SpectreViolationFilter(angr.exploration_techniques.ExplorationTechnique):
     if state.spectre.violation: return 'spectre_violation'
     return simgr.filter(state, **kwargs)
 
-# useful utility for interactive mode
+# Useful utilities for interactive mode
+
 def showbbASM(proj, bbaddr):
+    """
+    Show the x86 assembly for the basic block at the given address
+    """
     proj.factory.block(bbaddr).capstone.pp()
 
-# useful utility for interactive mode
 def showbbVEX(proj, bbaddr):
+    """
+    Show the VEX IR for the basic block at the given address
+    """
     proj.factory.block(bbaddr).vex.pp()
+
+def showBBHistory(proj, state, asm=True):
+    """
+    Show the entire history of basic blocks executed by the given state.
+        Note: shows the entire basic block, even if execution jumped out early.
+        (angr/VEX has a very different definition of "basic block" than you might think -
+            e.g. conditional jumps do not end basic blocks, only calls and unconditional ones.)
+        Look at the next basic block address to determine where execution left the current basic block.
+    asm: if True then show each block as x86 assembly instructions.
+        If False then show each block as VEX IR instructions.
+        If None then don't show block contents, only block addresses.
+    """
+    for bbaddr in state.history.bbl_addrs:
+        print("Basic block {}{}".format(hex(bbaddr), ":" if asm is not None else ""))
+        if asm: showbbASM(proj, bbaddr)
+        elif asm is not None: showbbVEX(proj, bbaddr)
+    print("Currently at instruction {}".format(hex(state.addr)))
+
+# 'Driver' functions
 
 def runTweetNaclCryptoSign(spec=True, window=None):
     """
