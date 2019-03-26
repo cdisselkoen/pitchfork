ef0e407bc6a5e8c6c185d9a2ddf69acec95219cd
Craig Disselkoen <craigdissel@gmail.com>
2019-03-26 11:39:59 -0700

    output histories to files

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@
 
 import claripy
 from taint import is_tainted
+from sys import stdout
 
 def isAst(x):
     return isinstance(x, claripy.ast.Base)
@@ -13,19 +14,21 @@ def describeAst(state, ast, checkTaint=True):
             else "{} (untainted, but with annotations {})".format(ast, ast.annotations) if ast.annotations \
             else "{} (untainted)".format(ast)
 
-def showbbASM(proj, bbaddr):
+def showbbASM(proj, bbaddr, file=stdout):
     """
     Show the x86 assembly for the basic block at the given address
+    file: where to print the output (must be an open file object, defaults to stdout)
     """
-    proj.factory.block(bbaddr).capstone.pp()
+    file.write(str(proj.factory.block(bbaddr).capstone)+'\n')
 
-def showbbVEX(proj, bbaddr):
+def showbbVEX(proj, bbaddr, file=stdout):
     """
     Show the VEX IR for the basic block at the given address
+    file: where to print the output (must be an open file object, defaults to stdout)
     """
-    proj.factory.block(bbaddr).vex.pp()
+    file.write(str(proj.factory.block(bbaddr).vex)+'\n')
 
-def showBBHistory(proj, state, asm=True):
+def showBBHistory(proj, state, asm=True, file=stdout):
     """
     Show the entire history of basic blocks executed by the given state.
         Note: shows the entire basic block, even if execution jumped out early.
@@ -35,9 +38,21 @@ def showBBHistory(proj, state, asm=True):
     asm: if True then show each block as x86 assembly instructions.
         If False then show each block as VEX IR instructions.
         If None then don't show block contents, only block addresses.
+    file: where to write the history (must be an open file object, defaults to stdout)
     """
     for bbaddr in state.history.bbl_addrs:
-        print("Basic block {}{}".format(hex(bbaddr), ":" if asm is not None else ""))
-        if asm: showbbASM(proj, bbaddr)
-        elif asm is not None: showbbVEX(proj, bbaddr)
-    print("Currently at instruction {}".format(hex(state.addr)))
+        file.write("Basic block {}{}\n".format(hex(bbaddr), ":" if asm is not None else ""))
+        if asm: showbbASM(proj, bbaddr, file=file)
+        elif asm is not None: showbbVEX(proj, bbaddr, file=file)
+    file.write("Currently at instruction {}\n".format(hex(state.addr)))
+
+def dumpHistories(proj, states, asm=True):
+    """
+    states: an iterable of states
+    asm: see notes on showBBHistory
+
+    Dump the histories of each of the states into files "history1.txt", "history2.txt", etc
+    """
+    for (i,state) in enumerate(states):
+        with open('history'+str(i)+'.txt', 'w') as f:
+            showBBHistory(proj, state, asm=asm, file=f)
